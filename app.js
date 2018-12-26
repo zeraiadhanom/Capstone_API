6e44695eee34cd29cb3393431fe980773930706b
Zerai Adhanom <zeraiadhanom@gmail.com>
2018-12-26 09:58:23 -0800

    changes updated to fix picture size

--- app.js
+++ app.js
@@ -6,30 +6,40 @@ const apiKey = '20b7f5cb0ac546e6bd0d8944a2b34cd1'
 
 const url = "https://api.nytimes.com/svc/movies/v2/reviews/search.json?critics-pick=Y&apikey=" + apiKey;
 
-function movieReviews(title) {
+function movieReviews(searchData) {
   
-  let fullURL = `${url}&query:${title}`
+  let fullURL = `${url}&query=${searchData}`;
+
   fetch(fullURL)
-  //console.log(fullURL)
     .then(response => response.json())
       .then(responseJson => {
-        //console.log(responseJson)
         displayResults(responseJson)
         })
-      .catch(error => alert('Something went wrong. Try again later.'));
+      .catch(error => console.log('Something went wrong. Try again later.'));
 }
 
-//why this is not working?
+  // looping throuh API array
  function displayResults(responseJson) {
-    for (let i = 0; i < responseJson.results.length; i++){
-   // console.log(responseJson.results[i].display_title);
-  
-  // Below line makes image is to link to anther image with high resolution
-   /* let newLink = responseJson.results[i].multimedia.src.split('-'); 
-    responseJson.results[i].multimedia.src = newLink[0] + "-superJumbo.jpg?quality=90&auto=webp"; */
+   //clean any displayed item;
+   $('.list').html('');
 
-         $('#results').html('')
-         $('#results').append(`<li class="list-group">
+       // Show a retry message if user enters invalid input or if item is not availble; 
+       
+   if(responseJson.results.length === 0) {
+     $('#notFound').removeClass('hidden')
+     $('#notFound').html('No movie availble with your search. Please try again.')
+     
+   }
+   
+     // looping through list of movies
+    for (let i = 0; i < responseJson.results.length; i++){
+
+      //format image link
+      let newLink = responseJson.results[i].multimedia.src.split('-'); 
+    
+ // display result on hml
+         
+         $('.list').append(`<li class="list-group">
          <img src="${responseJson.results[i].multimedia.src}" class="img-thumbnail" </li>
          <section>
          <h1>${responseJson.results[i].display_title}</h1>
@@ -39,23 +49,19 @@ function movieReviews(title) {
          <p>${"Updated date: " + " " + responseJson.results[i].date_updated}</p>
          <a href=${responseJson.results[i].link.url}>${responseJson.results[i].display_title}</a>
         </section>`);
-   
     }
 } 
 
-  
+// event listner; input search item and assign to searchData varibale.
 function watchForm() {
   $('.js-search-form').submit(function(event){
     event.preventDefault();
    const searchData = $('.js-query-title').val();
-     console.log(searchData);
     $('.js-query-title').val('');
 
    movieReviews(searchData);
   });
 } 
 
-//$(movieReviews);
-//$(displayResults);
 $(watchForm); 
 
